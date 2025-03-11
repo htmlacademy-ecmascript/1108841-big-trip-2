bbc90ba24c8eb1e7f54d19b8334d8e995da29765
Ilya Staroverov <51054402+ilya-star-dev@users.noreply.github.com>
2025-03-12 02:13:38 +0300

    д13

--- src/api/adapter.js
+++ src/api/adapter.js
@@ -38,7 +38,7 @@ const ToServerAdapter = {
     const adaptedPoint = {};
 
     adaptedPoint.id = point.id;
-    adaptedPoint['base_price'] = parseInt(point.basePrice, 10);
+    adaptedPoint['base_price'] = point.basePrice;
     adaptedPoint['date_from'] = point.dateFrom;
     adaptedPoint['date_to'] = point.dateTo;
     adaptedPoint.destination = point.destination;
