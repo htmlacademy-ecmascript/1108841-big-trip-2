8b6e86334ff3290a0344d826c9d67265efdc12a0
Ilya Staroverov <51054402+ilya-star-dev@users.noreply.github.com>
2025-03-14 20:50:54 +0300

    ref 0.2

--- src/api/api-service.js
+++ src/api/api-service.js
@@ -1,5 +1,4 @@
 import ApiService from '../framework/api-service.js';
-import { ApiErrorMessage } from '../const.js';
 
 export default class PointsApiService extends ApiService {
   constructor(endPoint, authorization) {
@@ -7,80 +6,46 @@ export default class PointsApiService extends ApiService {
   }
 
   get points() {
-    return this.#fetchPoints();
+    return this._load({ url: 'points' })
+      .then(ApiService.parseResponse);
   }
 
   get destinations() {
-    return this.#fetchDestinations();
+    return this._load({ url: 'destinations' })
+      .then(ApiService.parseResponse);
   }
 
   get offers() {
-    return this.#fetchOffers();
-  }
-
-  async #fetchPoints() {
-    try {
-      const response = await this._load({ url: 'points' });
-      return ApiService.parseResponse(response);
-    } catch (err) {
-      throw new Error(ApiErrorMessage.GET);
-    }
-  }
-
-  async #fetchDestinations() {
-    try {
-      const response = await this._load({ url: 'destinations' });
-      return ApiService.parseResponse(response);
-    } catch (err) {
-      throw new Error(ApiErrorMessage.GET);
-    }
-  }
-
-  async #fetchOffers() {
-    try {
-      const response = await this._load({ url: 'offers' });
-      return ApiService.parseResponse(response);
-    } catch (err) {
-      throw new Error(ApiErrorMessage.GET);
-    }
+    return this._load({ url: 'offers' })
+      .then(ApiService.parseResponse);
   }
 
   async updatePoint(point) {
-    try {
-      const response = await this._load({
-        url: `points/${point.id}`,
-        method: 'PUT',
-        body: JSON.stringify(point),
-        headers: new Headers({ 'Content-Type': 'application/json' })
-      });
-      return ApiService.parseResponse(response);
-    } catch (err) {
-      throw new Error(ApiErrorMessage.PUT);
-    }
+    const response = await this._load({
+      url: `points/${point.id}`,
+      method: 'PUT',
+      body: JSON.stringify(point),
+      headers: new Headers({ 'Content-Type': 'application/json' })
+    });
+
+    return ApiService.parseResponse(response);
   }
 
   async addPoint(point) {
-    try {
-      const response = await this._load({
-        url: 'points',
-        method: 'POST',
-        body: JSON.stringify(point),
-        headers: new Headers({ 'Content-Type': 'application/json' })
-      });
-      return ApiService.parseResponse(response);
-    } catch (err) {
-      throw new Error(ApiErrorMessage.POST);
-    }
+    const response = await this._load({
+      url: 'points',
+      method: 'POST',
+      body: JSON.stringify(point),
+      headers: new Headers({ 'Content-Type': 'application/json' })
+    });
+
+    return ApiService.parseResponse(response);
   }
 
   async deletePoint(id) {
-    try {
-      await this._load({
-        url: `points/${id}`,
-        method: 'DELETE',
-      });
-    } catch (err) {
-      throw new Error(ApiErrorMessage.DELETE);
-    }
+    return this._load({
+      url: `points/${id}`,
+      method: 'DELETE',
+    });
   }
 }
