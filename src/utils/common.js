c67ca64d4b6be26e03fb28de282c65a1be32747d
Ilya Staroverov <51054402+ilya-star-dev@users.noreply.github.com>
2025-03-14 22:21:31 +0300

    ref 0.7

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,14 +1,7 @@
-import { ArrayConfig, TokenConfig } from '../const.js';
-function getRandomArrayElement(items) {
-  return items[Math.floor(Math.random() * items.length)];
-}
-function capitalizeFirstLetter(word) {
-  if (!word) {
-    return '';
-  }
-  return word[ArrayConfig.FIRST_INDEX].toUpperCase() + word.slice(ArrayConfig.SECOND_INDEX);
-}
+import { TokenConfig } from '../const.js';
+
 const generateAuthToken = () => `Basic ${Array.from({length: TokenConfig.LENGTH}, () =>
   TokenConfig.CHARACTERS[Math.floor(Math.random() * TokenConfig.CHARACTERS.length)]
 ).join('')}`;
-export { getRandomArrayElement, capitalizeFirstLetter, generateAuthToken };
+
+export { generateAuthToken };
--- src/utils/filter.js
+++ src/utils/filter.js
@@ -4,22 +4,18 @@ import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 dayjs.extend(isSameOrBefore);
 dayjs.extend(isSameOrAfter);
+
 function isPointFuture(point) {
   return dayjs(point.dateFrom).isAfter(dayjs());
 }
+
 function isPointPresent(point) {
   const now = dayjs();
   return dayjs(point.dateFrom).isSameOrBefore(now) && dayjs(point.dateTo).isSameOrAfter(now);
 }
+
 function isPointPast(point) {
   return dayjs(point.dateTo).isBefore(dayjs());
 }
-function generateFilters() {
-  return {
-    [FilterType.EVERYTHING]: true,
-    [FilterType.FUTURE]: true,
-    [FilterType.PRESENT]: true,
-    [FilterType.PAST]: true,
-  };
-}
-export { generateFilters, isPointFuture, isPointPresent, isPointPast };
+
+export { isPointFuture, isPointPresent, isPointPast };
